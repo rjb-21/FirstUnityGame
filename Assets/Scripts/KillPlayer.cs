e7ed68e9776f5bc7df786f79c286099ae8bdcf2b
Radoslaw <71033043+rjb-21@users.noreply.github.com>
2022-08-21 17:50:29 +0200

    Lava and scene reset added
    Co-Authored-By: Radoslaw <71033043+rjb-21@users.noreply.github.com>

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Transform respawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+            //player.transform.position = respawnPoint.position;
+        }
+    }
+}
